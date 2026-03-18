ac6ec5a03b0f2401730345c042bbc9792b192518
deliewout <deliewout@gmail.com>
2026-03-18 14:43:38 +0100

    finished implementation of BFS
    renamed some of the variables so i dint get confused with the pseudocode from the ppt

--- Source/GameAIProg/GraphTheory/Algorithms/BFS.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/BFS.cpp
@@ -15,19 +15,29 @@ BFS::BFS(Graph* const pGraph)
 // TODO Breath First Search Algorithm searches for a path from the startNode to the destinationNode
 std::vector<Node*> BFS::FindPath(Node* const pStartNode, Node* const pDestinationNode) const
 {
-	std::vector<Node*> path = {};
-	std::queue<Node*> openList = {};
-	std::map<Node*, Node*> closedList = {};
-	openList.push(pStartNode);
-	while (!openList.empty())
+	std::vector<Node*> visited = {};
+	std::queue<Node*> nodeQueue = {};//openlist
+	nodeQueue.push(pStartNode);
+	std::map<Node*, Node*> parent = {};//closedlist
+	visited.push_back(pStartNode);
+	while (!nodeQueue.empty())
 	{
-		Node* pCurrentNode = openList.front();
-		openList.pop();
+		Node* pCurrentNode = nodeQueue.front();
+		nodeQueue.pop();
 		if (pCurrentNode == pDestinationNode)
-			return ReconstructPath(closedList, pStartNode, pDestinationNode);
-		//for(Connection* const)
+			return ReconstructPath(parent, pStartNode, pDestinationNode);
+		for (Connection* const pConnection : pGraph->FindConnectionsFrom(pCurrentNode->GetId()))
+		{
+			Node* pNextNode = pGraph->GetNode(pConnection->GetToId()).get();
+			if (parent.find(pCurrentNode) == parent.end())
+			{
+				visited.push_back(pNextNode);
+				parent[pNextNode] = pCurrentNode;
+				nodeQueue.push(pNextNode);
+			}
+		}
 	}
-	return path;
+	return std::vector<Node*>{};
 }
 
 std::vector<Node*> GameAI::BFS::ReconstructPath(std::map<Node*, Node*> parent, Node* pStartNode, Node* pEndNode) const
